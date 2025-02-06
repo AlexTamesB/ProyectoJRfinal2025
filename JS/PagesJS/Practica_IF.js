fad8b292e387cedef1feb1e47a4751e1c1ff71eb
AlexTamesB <jtamesb@vlacademy.com>
2025-02-05 18:12:30 -0600

    Se agrega verificacion de edad

--- JS/PagesJS/Practica_IF.js
+++ JS/PagesJS/Practica_IF.js
@@ -1,1 +1,26 @@
 console.log(`Funcionando Archivo: Practica_IF`);
+
+
+
+//caso1
+function verificarEdad(){
+
+
+    let edad = document.getElementById('edad').value; //value se agrega al final
+    let pais = document.getElementById('pais').value;
+
+
+    console.log(edad)
+    console.log(pais)
+
+
+    //IFELSE
+    
+if (edad >= 18) {
+    alert(`Eres mayor de edad`)
+} else { 
+    alert(`No eres mayor de edad`)
+    
+}
+
+}
