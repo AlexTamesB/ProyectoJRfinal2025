a4fdbcfd8295961e88fd104e03b4db18b545ea5e
AlexTamesB <jtamesb@vlacademy.com>
2025-02-05 18:39:14 -0600

    ingreso de verificacion de par o Impar

--- JS/PagesJS/Practica_IF.js
+++ JS/PagesJS/Practica_IF.js
@@ -3,7 +3,7 @@ console.log(`Funcionando Archivo: Practica_IF`);
 
 
 //caso1
-function verificarEdad(){
+function verificarEdad() {
 
 
     let edad = document.getElementById('edad').value; //value se agrega al final
@@ -15,12 +15,29 @@ function verificarEdad(){
 
 
     //IFELSE
-    
-if (edad >= 18) {
-    alert(`Eres mayor de edad`)
-} else { 
-    alert(`No eres mayor de edad`)
-    
+
+    if (edad >= 18) {
+        alert(`Eres mayor de edad`)
+    } else {
+        alert(`No eres mayor de edad`)
+
+    }
+
 }
 
+//parImpar
+function verificarParImpar() {
+
+    let numero3 = document.getElementById('numero3').value;
+
+    let numeroconvertido = parseInt(numero3)
+
+    if (numeroconvertido % 2 === 0) {
+
+        document.getElementById('mensajeParImpar').innerText = `El numero ${numeroconvertido} es par`;
+
+    } else {
+        document.getElementById('mensajeParImpar').innerText = `El numero ${numeroconvertido} es impar`;
+    }
+    console.log(numeroconvertido)
 }
